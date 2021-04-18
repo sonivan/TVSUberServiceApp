269b2e0705d019dfbe011ccf49f52bcab5e03393
Lucida Dragon <lucidadragon@gmail.com>
2021-04-18 18:47:33 -0400

    Implemented web request.

--- app/src/main/java/com/tc/uberserviceapp/api/API.kt
+++ app/src/main/java/com/tc/uberserviceapp/api/API.kt
@@ -1,17 +1,27 @@
 package com.tc.uberserviceapp.api
 
+import com.google.gson.Gson
 import com.tc.uberserviceapp.interfaces.BookingService
 import com.tc.uberserviceapp.interfaces.JobService
 import com.tc.uberserviceapp.interfaces.LoginService
 import com.tc.uberserviceapp.models.Booking
 import com.tc.uberserviceapp.models.Job
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
 
 class API : LoginService, JobService, BookingService {
     override val isLoggedIn: Boolean
-        get() = TODO("Not yet implemented")
+        get() = _isLoggedIn
+
+    private var _isLoggedIn = false
 
     override fun login(email: String, password: String): Boolean {
-        TODO("Not yet implemented")
+
+
+        return isLoggedIn
     }
 
     override fun logout() {
@@ -43,4 +53,30 @@ class API : LoginService, JobService, BookingService {
         TODO("Not yet implemented")
     }
 
+    private fun <T> call(target: String, method: String, endpoint: String, data: T?): Response {
+        var json: String = ""
+
+        if (data != null)
+        {
+            json = Gson().toJson(data)
+        }
+
+        val url = URL(target + endpoint)
+
+        with(url.openConnection() as HttpURLConnection) {
+            requestMethod = method
+
+            if (json.length > 0) {
+                val outbound = OutputStreamWriter(outputStream)
+                outbound.write(json)
+                outbound.flush()
+            }
+
+            BufferedReader(InputStreamReader(inputStream)).use {
+                return Response(responseCode, it.readText())
+            }
+        }
+    }
+
+    private class Response(val code: Int, val json: String)
 }
