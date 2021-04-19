41a3111d850efff086d88331c0d8785cacaf59c7
Lucida Dragon <lucidadragon@gmail.com>
2021-04-18 23:39:17 -0400

    Added viewmodels to activities.

--- app/src/main/java/com/tc/uberserviceapp/LoginActivity.kt
+++ app/src/main/java/com/tc/uberserviceapp/LoginActivity.kt
@@ -1,13 +1,17 @@
 package com.tc.uberserviceapp
 
+import ViewModels.LoginVM
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tc.uberserviceapp.api.API
 import com.tc.uberserviceapp.databinding.ActivityJobWaitingScreenBinding
 import com.tc.uberserviceapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+
+    private var ViewModel = LoginVM(API.Instance)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/tc/uberserviceapp/RegisterActivity.kt
+++ app/src/main/java/com/tc/uberserviceapp/RegisterActivity.kt
@@ -1,13 +1,17 @@
 package com.tc.uberserviceapp
 
+import ViewModels.LoginVM
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tc.uberserviceapp.api.API
 import com.tc.uberserviceapp.databinding.ActivityMainBinding
 import com.tc.uberserviceapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+
+    private var ViewModel = LoginVM(API.Instance)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
